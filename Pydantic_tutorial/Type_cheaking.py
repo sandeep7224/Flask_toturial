a69cbc5eda4a2cac01ff38b43c823f1bb540c612
sandeep Gupta <155440963+sandeep7224@users.noreply.github.com>
2026-01-29 18:51:10 +0530

    Add files via upload

---
+++ Pydantic_tutorial/Type_cheaking.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import List, Dict, Optional
+
+class Student(BaseModel):
+    name : str
+    age: int
+    gender: Optional[str] = None
+    married: bool
+    family_member: List[str]
+    contact_detail: Dict[str, str]
+
+def insert(stud: Student):
+    print(stud.name)
+    print(stud.age)
+    print(stud.gender)
+    print(stud.married)
+    print(stud.family_member)
+    print(stud.contact_detail)
+
+    print("data is inserted successfully")
+
+
+Student_detail = {"name": "sandip", "age": "7", "married": True, "family_member": ["father", "mother"], "contact_detail":{"phone": "7234325", "email": "sandip435@gmail.com"}}
+
+obj = Student(**Student_detail)   #create object for the class where passing detail into the class student to validate
+
+insert(obj)
